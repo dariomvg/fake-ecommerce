5fdb4b1c3dd178fcc38fe0ee84cff60134800366
dariomvg <contact.dario.info@gmail.com>
2024-01-04 18:34:08 -0300

    agregamos nuevo despliegue

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { Menu } from './Routes/Menu'
 import { SectionProducts } from './Routes/SectionProducts'
 import { SectionMain } from './Routes/SectionMain'
 import { Cart } from './Routes/Cart'
-import { Page404 } from './Routes/Page404'
 import './styles/App.scss'
 
 function App() {
 
   return (
     <section className='wrapper'>
-      <Router>
+      <HashRouter>
         <Menu />
         <Routes>
           <Route path='/' element={<SectionMain />} />
           <Route path='/products' element={<SectionProducts />} />
           <Route path='/cart' element={<Cart />} />
-          <Route path='*' element={<Page404 />} />
         </Routes>
-      </Router>
+      </HashRouter>
     </section>
   )
 }
